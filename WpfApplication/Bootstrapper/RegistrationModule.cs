569d7beb70796cc551cc9a5bf52ca016a180b93a
Sacarlite <vlade@LAPTOP-ATRNCTJ6>
2024-06-29 19:57:14 +0300

    Добавлены регистрационные модули

---
+++ WpfApplication/Bootstrapper/RegistrationModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootstrapper
+{
+    public class RegistrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Регистрация фабрики окон
+            //TODO зарегистрировать фабрику
+            //Регистрация зависимости
+        }
+    }
+}
---
+++ WpfApplication/Vievs/RegistrationModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vievs
+{
+    public class RegistrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Регистрация окон
+            //TODO Зарегистрировать окна
+            //Регистрация менеджеров
+            //TODO Зарегистрировать менеджер
+        }
+    }
+}
