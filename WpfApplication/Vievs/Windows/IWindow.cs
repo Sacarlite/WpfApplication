4ffd4cde3a2ad4ce628eb7a65e4896119b884591
Sacarlite <vlade@LAPTOP-ATRNCTJ6>
2024-06-29 20:06:44 +0300

    Создано главное окно

---
+++ WpfApplication/Vievs/Windows/IWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vievs.Windows
+{
+    public interface IWindow
+    {
+        void Show();
+        void Close();
+        bool Activate();
+
+        event CancelEventHandler Closing;
+        event EventHandler Closed;
+    }
+}
